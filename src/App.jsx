02795a2adb7997582849a717df246da4461f1db2
vinayakpandeycode <vinayakpandey2027@gmail.com>
2025-09-09 06:36:30 +0530

    Initial frontend commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bot from './component/Bot'
+
+function App() {
+
+  // javascript code goes here!!
+
+  return (
+    <div><Bot/></div>
+  )
+}
+
+export default App
